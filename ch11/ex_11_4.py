5b090a8b23b93c40a6392d9c39cbc35fe16f1ed4
gp22 <paulgarcia@protonmail.com>
2016-09-04 15:31:41 +0700

    Updated files

--- ch11/ex_11_4.py
+++ ch11/ex_11_4.py
@@ -1,4 +1,19 @@
 # Write a program that asks the user for a sentence in English
 # and then translates that sentence to Pirate.
 
-english2pirate = {'sir': 'matey', 'hotel': 'fleabag inn', 'student': 'swabbie'}
+# create the pirate dictionary
+english2pirate = {'sir': 'matey', 'hotel': 'fleabag inn', 'student': 'swabbie',
+                  'boy': 'matey', 'madam': 'proud beauty',
+                  'professor': 'foul blaggart', 'restaurant': 'galley',
+                  'your': 'yer', 'excuse': 'arr', 'students': 'swabbies',
+                  'are': 'be', 'lawyer': 'foul blaggart', 'the': "th'",
+                  'restroom': 'head', 'my': 'me', 'hello': 'avast', 'is': 'be',
+                  'man': 'matey'}
+
+user_input = input('Enter a sentence > ')
+for word in user_input.split():
+    if word in english2pirate:
+        print(english2pirate[word])
+    else:
+        print(word)
+
